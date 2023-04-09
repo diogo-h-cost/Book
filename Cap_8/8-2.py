cf7e4a99975bbcaa883b44fec3c375c749ce0aea
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-04-09 20:03:15 -0300

    Alteracoes >> 8-2.py

--- Cap_8/8-2.py
+++ Cap_8/8-2.py
@@ -1,9 +1,8 @@
+def multiplo(n1, n2):
+    if n1 / 2 == n2 or n1 == n2:
+        return True
+    return False
+
 n1 = int(input())
 n2 = int(input())
-Multi = True
-
-if n1 / 2 == n2 or n1 == n2:
-    print(f"multiplo({n1}, {n2}) == {Multi}")
-else:
-    multi = False
-    print(f"multiplo({n1}, {n2}) == {multi}")
+print(f"multiplo({n1}, {n2}) == {multiplo(n1, n2)}")
