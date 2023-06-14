085ff889f0c46b25b175d6b5d3bb6541a72654f3
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-06-14 16:12:49 -0300

    9-30

---
+++ Cap_9/9-30.py
@@ -0,0 +1,23 @@
+filmes = {
+    "Drama": ["Cidadão Kane", "O Poderoso Chefão"],
+    "Comédia": ["Tempos Modernos", "American Pie", "Dr. Dolittle"],
+    "Policial": ["Chuva Negra", "Desejo de Matar", "Difícil de Matar"],
+    "Guerra": ["Rambo", "Platoon", "Tora!Tora!Tora"]
+}
+
+with open("filmes.html", "w", encoding="utf-8") as pagina:
+    pagina.write("""
+    <!DOCTYPE html>
+    <html lang="pt=BR">
+    <head>
+    <meta charset="utf-8">
+    <title>Filmes</title>
+    </head>
+    <body>
+    <ul>
+    """)
+    for c, v in filmes.items():
+        pagina.write(f"<h1>{c}</h1>\n")
+        for e in v:
+            pagina.write(f"<li><p>{e}</p></li>\n")
+    pagina.write("</ul></body></html>")
