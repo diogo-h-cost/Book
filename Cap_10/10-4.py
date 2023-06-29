2dca3e2d8a7d829a12f9686832a8c91192c186a1
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-06-29 13:49:49 -0300

    10-4

---
+++ Cap_10/10-4.py
@@ -0,0 +1,17 @@
+class televisao:
+    def __init__(self, min = 2, max = 14):
+        self.ligada = False
+        self.canal = 2
+        self.cmin = min
+        self.cmax = max
+    def muda_canal_para_baixo(self):
+        if self.canal - 1 >= self.cmin:
+            self.canal -= 1
+    def muda_canal_para_cima(self):
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
+
+tv = televisao() #Min e Max opcionais
+
+print(tv.cmin)
+print(tv.cmax)
