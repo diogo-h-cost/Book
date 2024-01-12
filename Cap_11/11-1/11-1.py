4f0d91decb3a4ab74ea8dcdef6cf16c59cf26840
Diogo Costa <diogo.henrique.cdl@gmail.com>
2024-01-12 15:54:57 -0300

    11-1

---
+++ Cap_11/11-1/11-1.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conexao = sqlite3.connect("precos.db")
+cursor = conexao.cursor()
+
+cursor.execute("CREATE TABLE precos(nome TEXT, preco REAL)")
+
+cursor.execute("INSERT INTO precos(nome, preco) VALUES(?, ?)", ("Arroz", 10.7))
+
+conexao.commit()
+cursor.close()
+conexao.close()
